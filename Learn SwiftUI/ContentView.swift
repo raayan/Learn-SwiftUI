4a5fa376a918307efd16960012d2e854128d43b3
Raayan Pillai <raayanp01@gmail.com>
2019-07-10 23:57:51 -0400

    Initial Commit

---
+++ Learn SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Learn SwiftUI
+//
+//  Created by Raayan Pillai on 7/10/19.
+//  Copyright © 2019 Raayan Pillai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
